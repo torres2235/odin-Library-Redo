3ab708ecc193b60dee773a40b11c3e10e875c754
Torres2235 <Torres2235@gmail.com>
2024-02-07 19:02:45 -0800

    initial layout styling

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
   // do stuff here
+  const book = new Book(title, author, pages, read)
+
+  myLibrary.push(book)
 }
+
+//initial book to start our list           
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
+
+myLibrary.push(theHobbit);
