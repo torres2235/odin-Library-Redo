13111da19ef050f0ec88678478acf471ec442c4b
Torres2235 <Torres2235@gmail.com>
2024-02-08 17:49:21 -0800

    changes to addBookToLibrary funciton

--- index.js
+++ index.js
@@ -8,14 +8,45 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  alert("Hello World");
+});
+
+let n = 0;
+
 function addBookToLibrary(title, author, pages, read) {
   // do stuff here
-  const book = new Book(title, author, pages, read)
+  const book = new Book(title, author, pages, read) //make new Book object
+  myLibrary.push(book) //add book into myLibrary
 
-  myLibrary.push(book)
+  const main = document.querySelector("main");
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+  main.appendChild(card);
+
+  
+  const cTitle = document.createElement("p");
+  cTitle.classList.add("title")
+  cTitle.innerText = title;
+  card.appendChild(cTitle);
+
+  const cAuthor = document.createElement("p");
+  cAuthor.classList.add("author")
+  cAuthor.innerText = author;
+  card.appendChild(cAuthor);
+
+  const cPages = document.createElement("p");
+  cPages.classList.add("pages")
+  cPages.innerText = pages;
+  card.appendChild(cPages);
+
+  const cRead = document.createElement("p");
+  cRead.classList.add("read")
+  cRead.innerText = read;
+  card.appendChild(cRead);
 }
 
 //initial book to start our list           
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
-
-myLibrary.push(theHobbit);
+addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, false);
