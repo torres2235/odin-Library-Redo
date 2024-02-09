ffedbd7597eca0b8d09a83e00b7ad757835fd00f
Torres2235 <Torres2235@gmail.com>
2024-02-09 14:45:27 -0800

    styling header and cards

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {
-  alert("Hello World");
-});
-
-let n = 0;
-
 function addBookToLibrary(title, author, pages, read) {
   // do stuff here
   const book = new Book(title, author, pages, read) //make new Book object
@@ -27,26 +20,41 @@ function addBookToLibrary(title, author, pages, read) {
   main.appendChild(card);
 
   
-  const cTitle = document.createElement("p");
-  cTitle.classList.add("title")
+  const cTitle = document.createElement("h2");
+  cTitle.classList.add("title");
   cTitle.innerText = title;
   card.appendChild(cTitle);
 
   const cAuthor = document.createElement("p");
-  cAuthor.classList.add("author")
-  cAuthor.innerText = author;
+  cAuthor.classList.add("author");
+  cAuthor.innerText = "Author: " + author;
   card.appendChild(cAuthor);
 
   const cPages = document.createElement("p");
-  cPages.classList.add("pages")
-  cPages.innerText = pages;
+  cPages.classList.add("pages");
+  cPages.innerText = "Pages: " + pages;
   card.appendChild(cPages);
 
-  const cRead = document.createElement("p");
-  cRead.classList.add("read")
-  cRead.innerText = read;
+  const cRead = document.createElement("button");
+  if(read == true) {
+    cRead.classList.add("read");
+    cRead.innerText = "Read";
+  } else {
+    cRead.classList.add("notRead");
+    cRead.innerText = "Not Read";
+  }
   card.appendChild(cRead);
+
+  const del = document.createElement("button");
+  del.setAttribute('id','del');
+  del.innerText = 'X';
+  card.appendChild(del);
 }
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  alert("Hello World");
+});
 
 //initial book to start our list           
 addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, false);
