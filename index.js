3097d80529595e8f496d8542cdec59d78c81d061
Torres2235 <Torres2235@gmail.com>
2024-02-19 19:02:59 -0800

    converted book function into book class

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  // the constructor...
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+class Book { //converted our book function into a book class
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }; 
 
-function addBookToLibrary(title, author, pages, read) {
-  // do stuff here
-  const book = new Book(title, author, pages, read) //make new Book object
-  myLibrary.push(book) //add book into myLibrary
+    add() {
+        //const book = new Book(title, author, pages, read);
+        myLibrary.push([this.title, this.author, this.pages, this.read]);
 
-  const main = document.querySelector("main");
+        const main = document.querySelector("main");
 
-  const card = document.createElement("div");
-  card.classList.add("card");
-  main.appendChild(card);
+        const card = document.createElement("div");
+        card.classList.add("card");
+        main.appendChild(card);
 
-  
-  const cTitle = document.createElement("h2");
-  cTitle.classList.add("title");
-  cTitle.innerText = title;
-  card.appendChild(cTitle);
+        
+        const cTitle = document.createElement("h2");
+        cTitle.classList.add("title");
+        cTitle.innerText = this.title;
+        card.appendChild(cTitle);
 
-  const cAuthor = document.createElement("p");
-  cAuthor.classList.add("author");
-  cAuthor.innerText = "Author: " + author;
-  card.appendChild(cAuthor);
+        const cAuthor = document.createElement("p");
+        cAuthor.classList.add("author");
+        cAuthor.innerText = "Author: " + this.author;
+        card.appendChild(cAuthor);
 
-  const cPages = document.createElement("p");
-  cPages.classList.add("pages");
-  cPages.innerText = "Pages: " + pages;
-  card.appendChild(cPages);
+        const cPages = document.createElement("p");
+        cPages.classList.add("pages");
+        cPages.innerText = "Pages: " + this.pages;
+        card.appendChild(cPages);
 
-  const cRead = document.createElement("button");
-  cRead.classList.add("read");
-  if(read) {
-    cRead.innerText = "Read";
-    cRead.style.background = "#15803d";
-  } else {
-    cRead.innerText = "Not Read";
-    cRead.style.background = "#ca8a04";
-  }
-  card.appendChild(cRead);
-  cRead.addEventListener('click', () => {
-    read = !read;
-    if(read) {
-        cRead.innerText = "Read";
-        cRead.style.background = "#15803d";
-    } else {
-        cRead.innerText = "Not Read";
-        cRead.style.background = "#ca8a04";
+        const cRead = document.createElement("button");
+        cRead.classList.add("read");
+        if(this.read) {
+            cRead.innerText = "Read";
+            cRead.style.background = "#15803d";
+        } else {
+            cRead.innerText = "Not Read";
+            cRead.style.background = "#ca8a04";
+        }
+        card.appendChild(cRead);
+        cRead.addEventListener('click', () => {
+            this.read = !this.read;
+            if(this.read) {
+                cRead.innerText = "Read";
+                cRead.style.background = "#15803d";
+            } else {
+                cRead.innerText = "Not Read";
+                cRead.style.background = "#ca8a04";
+            }
+        });
+
+        const del = document.createElement("button");
+        del.classList.add("del")
+        del.innerHTML = '&times;';
+        card.appendChild(del);
+        del.addEventListener('click', () => {
+            card.remove();
+        });
     }
-  });
-
-  const del = document.createElement("button");
-  del.classList.add("del")
-  del.innerHTML = '&times;';
-  card.appendChild(del);
-  del.addEventListener('click', () => {
-    card.remove();
-  });
-}
+};
 
 const submit = document.getElementById('submit');
 submit.addEventListener('click', () => {
@@ -74,7 +74,8 @@ submit.addEventListener('click', () => {
     const check = document.getElementById('check').checked;
 
     if(title.value && author.value && pages.value) {
-        addBookToLibrary(title.value, author.value, pages.value, check);
+        let book = new Book(title.value, author.value, pages.value, check);
+        book.add();
         title.value='';
         author.value='';
         pages.value='';
@@ -128,5 +129,6 @@ function closeModal(modal) {
     }
 }
 
-//initial book to start our list           
-addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, false);
+//initial book to start our list 
+const b = new Book('The Hobbit', 'J.R.R Tolkien', 295, false);
+b.add();
